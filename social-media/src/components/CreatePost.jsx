d26635a60b982d859b91fbe78d8d669919911c6b
AmanShakya0018 <amanshakya1808@gmail.com>
2024-03-17 20:27:10 +0530

    Project

---
+++ social-media/src/components/CreatePost.jsx
@@ -0,0 +1,103 @@
+import { useContext } from "react";
+import { useRef } from "react";
+import { PostList } from "../store/post-list-store";
+
+const CreatePost = () => {
+  const { addPost } = useContext(PostList);
+
+  const userIDElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const reactionsElement = useRef();
+  const tagsElement = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userID = userIDElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postBody = postBodyElement.current.value;
+    const reactions = reactionsElement.current.value;
+    const tags = tagsElement.current.value.split(" ");
+
+    userIDElement.current.value = "";
+    postTitleElement.current.value = "";
+    postBodyElement.current.value = "";
+    reactionsElement.current.value = "";
+    tagsElement.current.value = "";
+
+    addPost(userID, postTitle, postBody, reactions, tags);
+  };
+
+  return (
+    <form className="create-post" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="userID" className="form-label">
+          Enter your USER ID here
+        </label>
+        <input
+          type="text"
+          ref={userIDElement}
+          className="form-control"
+          id="userID"
+          placeholder="USER ID"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          ref={postTitleElement}
+          className="form-control"
+          id="title"
+          placeholder="Write title for your post"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Enter ypur content
+        </label>
+        <textarea
+          rows={4}
+          type="text"
+          ref={postBodyElement}
+          className="form-control"
+          id="body"
+          placeholder="Write more about your post"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Number of reactions
+        </label>
+        <input
+          type="text"
+          ref={reactionsElement}
+          className="form-control"
+          id="reactions"
+          placeholder="How many peoples reacted to this post"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter your hashtags
+        </label>
+        <input
+          type="text"
+          ref={tagsElement}
+          className="form-control"
+          id="tags"
+          placeholder="Please enter your tags separated by space"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </form>
+  );
+};
+
+export default CreatePost;
